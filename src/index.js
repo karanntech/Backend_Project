4184ee8ea4f2da46c00497fd39b0ea4956d2a9bc
karann09 <karankumar011kp@gmail.com>
2024-08-24 22:22:16 +0530

    utils setup

--- src/index.js
+++ src/index.js
@@ -8,4 +8,12 @@ dotenv.config({
 
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,() => {
+        console.log(`Server is running at Port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MongoDB connection Failed!!!", error);
+})
