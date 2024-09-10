25671870796d8b48bea5d8c2c2c9fefd0ed23f77
karann09 <karankumar011kp@gmail.com>
2024-09-10 15:59:49 +0530

    added delete on cloudinary function

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -26,4 +26,17 @@ const uploadOnCloudinary = async (localFilePath) => {
     }
 }
 
-export {uploadOnCloudinary};
+const deleteOnCloudinary = async (public_id, resource_type="image") => {
+    try {
+        if (!public_id) return null;
+
+        //delete file from cloudinary
+        const result = await cloudinary.uploader.destroy(public_id, {
+            resource_type: `${resource_type}`
+        });
+    } catch (error) {
+        return error;
+    }
+};
+
+export {uploadOnCloudinary, deleteOnCloudinary};
