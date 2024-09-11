531db91d420c114baf57df9786586eccf497dcdc
karann09 <karankumar011kp@gmail.com>
2024-09-11 16:38:08 +0530

    added routes and comtroller for videos

---
+++ src/routes/video.routes.js
@@ -0,0 +1,41 @@
+import { Router } from "express";
+import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import {
+    getAllVideos,
+    publishAVideo,
+    deleteVideo,
+    updateVideo,
+    togglePublishStatus
+} from "../controllers/video.controller.js";
+
+const router = Router();
+
+// router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+
+router
+    .route("/")
+    .get(getAllVideos)
+    .post(
+        verifyJWT,
+        upload.fields([
+            {
+                name: "videoFile",
+                maxCount: 1
+            },
+            {
+                name: "thumbnail",
+                maxCount: 1
+            }
+        ]),
+        publishAVideo
+    );
+
+router
+    .route("/v/:videoId")
+    .delete(verifyJWT, deleteVideo)
+    .patch(verifyJWT, upload.single("thumbnail"), updateVideo);
+
+router.route("/toggle/publish/:videoId").patch(verifyJWT, togglePublishStatus);
+
+export default router;
